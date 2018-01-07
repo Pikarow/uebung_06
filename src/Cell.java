801459284d564887b87f1e52698b73eab6c54e8b
Pikarow <hu.rodriguez@gmx.de>
2018-01-07 18:53:25 +0100

    Initial commit

---
+++ src/Cell.java
@@ -0,0 +1,18 @@
+
+
+public class Cell {
+
+    public int content;
+    public Cell next;
+    public Cell prev;
+
+    public Cell(int dataValue) {
+        content = dataValue;
+        next = null;
+    }
+
+
+
+}
+
+
